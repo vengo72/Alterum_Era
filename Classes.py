cc223333176bdec362a4d2e59b63e62ef3874734
UnusualName <ars.plmr@gmail.com>
2022-12-15 22:12:19 +0500

    add exemplary base

---
+++ Classes.py
@@ -0,0 +1,49 @@
+import pygame
+
+
+class World:
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class Board:
+    def __init__(self, width, height, *args, **kwargs):
+        self.board = [[Cell(x, y) for x in range(width)] for y in range(height)]
+        self.width = width
+        self.height = height
+        self.left = args[0]
+        self.top = args[1]
+        self.cell_size = args[2]
+
+    def render(self, scr):
+        for i in range(self.width):
+            for j in range(self.height):
+                pygame.draw.rect(scr, 'white', (
+                    (i * self.cell_size), (j * self.cell_size),
+                    self.cell_size, self.cell_size), 1)
+
+
+class Cell:
+    def __init__(self, x, y, *args, **kwargs):
+        self.x = x
+        self.y = y
+
+
+a = Board(10, 10, 100, 100, 20)
+print(a.board)
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Alterum Era')
+    size = window_width, window_height = 800, 800
+    screen = pygame.display.set_mode(size)
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        a.render(screen)
+        pygame.display.flip()
+
